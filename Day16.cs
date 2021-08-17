09a977a25c262f7aedf4ee0490f0f82249d24183
mselek <63142764+mselek@users.noreply.github.com>
2021-08-17 17:25:20 +0300

    Exceptions - String to Integer

---
+++ Day16.cs
@@ -0,0 +1,33 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+        
+        try
+        {
+           var i = int.Parse(S);
+           Console.Write(i); 
+        }
+        catch
+        {
+            Console.Write("Bad String");
+        }
+    }
+}
